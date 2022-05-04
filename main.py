880a3f9d01dea797799cfddd3f40cfdeac7b6095
K1Nf <nikitka.repin.2017@mail.ru>
2022-05-04 20:54:58 +0500

    Task 2 done

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+#task2
+spisok1 = input("Введите список строк через пробел: ").split()
+number = int(input("Введите номер позиции для новой строки: "))
+newline = input("Введите новую строку: ")
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-print("kto chitaet tot lox")
+def secondtaskmethod(spisok: list, number: int, line: str):
+    for x in range(len(spisok)):
+        if x == number:
+            spisok[x] = line
+            return spisok
+    return "paste operation is not possible"
+
+
+print({secondtaskmethod(spisok1, number, newline)})
